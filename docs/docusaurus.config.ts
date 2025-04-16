df3509ec998d002e8bcef6285c7c0bc16b54d513
fossdd <fossdd@pwned.life>
2025-04-16 05:23:26 +0200

    docs/blog: remove (#273)
    still leftovers from the docusaurus template

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -126,10 +126,6 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/TecharoHQ/anubis',
             },
